bb3473dc032bdf75c98fc6c0fce1e80a8a282a07
mish26 <miyahra@gmail.com>
2023-01-15 16:32:39 +0900

    login処理追加

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,6 @@ package config
 
 type Config struct {
 	URL      string `env:"URL"`
-	UserID   int    `env:"USER_ID"`
-	Password int    `env:"PASSWORD"`
+	UserID   string `env:"USER_ID"`
+	Password string `env:"PASSWORD"`
 }
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-rod/rod/lib/input"
 	"github.com/joho/godotenv"
 	"sports-booking/cmd/config"
+	"time"
 )
 
 func main() {
@@ -19,7 +20,24 @@ func main() {
 		fmt.Printf("%+v\n", err)
 	}
 
-	browser := rod.New().MustConnect()
+	// Headless runs the browser on foreground, you can also use flag "-rod=show"
+	// Devtools opens the tab in each new tab opened automatically
+	//l := launcher.New().
+	//	Headless(false).
+	//	Devtools(true)
+	////
+	//defer l.Cleanup() // remove launcher.FlagUserDataDir
+	////
+	//url := l.MustLaunch()
+
+	// Trace shows verbose debug information for each action executed
+	// SlowMotion is a debug related function that waits 2 seconds between
+	// each action, making it easier to inspect what your code is doing.
+	browser := rod.New().
+		//ControlURL(url).
+		//Trace(true).
+		SlowMotion(2 * time.Second).
+		MustConnect()
 
 	defer browser.MustClose()
 
@@ -27,9 +45,8 @@ func main() {
 
 	page.MustElement("#login").MustType(input.Enter)
 
-	page.MustElement("#userid").MustInput(string(cfg.UserID))
-	page.MustElement("#userid").MustInput(string(cfg.Password)).MustType(input.Enter)
-
+	page.MustElement("#userid").MustInput(cfg.UserID)
+	page.MustElement("#passwd").MustInput(cfg.Password).MustType(input.Enter)
 	text := page.MustElement("#childForm").MustText()
 
 	fmt.Println(text)
